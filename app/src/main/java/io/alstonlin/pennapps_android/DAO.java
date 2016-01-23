ae11a46d8d0942692f57a4781d50e92279e23804
Alston Lin <alstonlin812@gmail.com>
2016-01-22 21:16:06 -0500

    Added stuff in DAO

--- app/src/main/java/io/alstonlin/pennapps_android/DAO.java
+++ app/src/main/java/io/alstonlin/pennapps_android/DAO.java
@@ -1,4 +1,15 @@
 package io.alstonlin.pennapps_android;
 
 public class DAO {
+    // TODO: Add paths as constants here
+    private static DAO instance;
+    private DAO(){
+        // TODO: Add stuff to establish connections
+    }
+    public static DAO getInstance(){
+        if (instance == null) instance = new DAO();
+        return instance;
+    }
+    // TODO: Add public non-static methods to use as interface
+    // TODO: Add private non-static methods to externalize code used for all HTTP actions
 }
